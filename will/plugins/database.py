c9e158b8441361a4443a90f587508c589b5fe853
Casey Reid <32307917+packetchaos@users.noreply.github.com>
2022-03-24 11:34:17 -0700

    Add files via upload

---
+++ will/plugins/database.py
@@ -0,0 +1,51 @@
+import sqlite3
+from sqlite3 import Error
+import click
+
+
+def new_db_connection(db_file):
+    # create a connection to our database
+    conn = None
+    try:
+        # A database file will be created if one doesn't exist
+        conn = sqlite3.connect(db_file, timeout=10.0)
+    except Error as E:
+        click.echo(E)
+    return conn
+
+
+def create_table(conn, table_information):
+    try:
+        c = conn.cursor()
+        c.execute('pragma journal_mode=wal;')
+        c.execute(table_information)
+    except Error as e:
+        click.echo(e)
+
+
+def db_query(statement):
+
+    database = r"willit.db"
+    query_conn = new_db_connection(database)
+    with query_conn:
+        cur = query_conn.cursor()
+        cur.execute('pragma journal_mode=wal;')
+        cur.execute('pragma cache_size=-10000;')
+        cur.execute('PRAGMA synchronous = OFF')
+        cur.execute('pragma threads=4')
+        cur.execute(statement)
+
+        data = cur.fetchall()
+    query_conn.close()
+
+    return data
+
+
+def drop_tables(conn, table):
+    try:
+        drop_table = '''DROP TABLE {}'''.format(table)
+        cur = conn.cursor()
+        cur.execute('pragma journal_mode=wal;')
+        cur.execute(drop_table)
+    except Error:
+        pass
